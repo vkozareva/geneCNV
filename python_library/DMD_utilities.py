14546799efc3aaae06c699c437a0e89503dd4db9
msilver727 <maxsilver@genepeeks.com>
2017-03-08 18:20:56 -0500

    add parameters to get DMD exons merged for limiting to just primary transcript vs all transcripts, and only coding regions vs full exons
    also removed unnecessary get_exon_num function

--- python_library/DMD_utilities.py
+++ python_library/DMD_utilities.py
@@ -1,41 +1,24 @@
 from genepeeks.common import utilities as util
 
 
-def get_DMD_exons_merged(min_dist=200, print_merged_exons=True):
+def get_DMD_exons_merged(min_dist=200, only_primary_transcript=True, only_coding_regions=False, print_merged_exons=True):
     """ Get DMD exons from primary DMD transcript in mongo, and merge all exons within min_dist of each other """
     DMD_ensembl = util.Mongo.get_collection_data('gene', wanted_db='prod', query={'_id': 'DMD'}, find_one=True,
                                                  single_field='ensembl', required=True, print_stats=False)
-    DMD_primary_transcript_d = util.get_nested_value(DMD_ensembl, ('is_primary', 'transcripts', 'is_primary'), required=True)
-    DMD_exons = DMD_primary_transcript_d['exons']
-
-    # Add coding start and end to first and last exons
-    DMD_coding_region = DMD_primary_transcript_d['translation']
-    DMD_exons[0]['coding_start'] = DMD_coding_region['start']
-    DMD_exons[-1]['coding_end'] = DMD_coding_region['end']
+    if only_primary_transcript:
+        DMD_primary_transcript_d = util.get_nested_value(DMD_ensembl, ('is_primary', 'transcripts', 'is_primary'), required=True)
+        DMD_exons = util.get_transcript_coding_regions(DMD_primary_transcript_d) if only_coding_regions else DMD_primary_transcript_d['exons']
+    else:
+        # If not only_primary_transcript, combine exon info from all transcripts. Optionally restrict to just coding_regions
+        DMD_exons = []
+        for transcript_d in DMD_ensembl[0]['transcripts']:
+            DMD_exons += util.get_transcript_coding_regions(transcript_d) if only_coding_regions else transcript_d['exons']
+        DMD_exons = util.merge_intervals(DMD_exons)
 
     # Merge all exons within min_dist of each other and use exon_labels to keep track of which exons were merged
     DMD_exons_merged = util.merge_intervals(DMD_exons, min_dist=min_dist, include_index=True)
     exon_labels = ['Ex' + exon['index'] for exon in DMD_exons_merged]
     if print_merged_exons:
-        print 'The following exons were merged: {}'.format([exon['index'] for exon in DMD_exons_merged if not exon['index'].isdigit()])
+        print 'There are {} merged exons, with the following exons being merged: {}'.format(
+            len(DMD_exons_merged), [exon['index'] for exon in DMD_exons_merged if not exon['index'].isdigit()])
     return DMD_exons_merged, exon_labels
-
-
-def get_exon_num(read_start, read_end, exon_list, skipped_counts=None):
-    """ Determine which exon a read overlaps with, if any """
-    exon_num = None
-    for i, exon in enumerate(exon_list):
-        if exon['end'] < read_start:
-            continue
-        else:
-            if exon['start'] <= read_end:
-                # Check if the read pair falls in two exons, and skip if it does.
-                if i != (len(exon_list) - 1) and exon_list[i + 1]['start'] <= read_end:
-                    if skipped_counts is not None:
-                        util.add_to_dict(skipped_counts, 'in_two_exons')
-                else:
-                    exon_num = i
-            elif skipped_counts is not None:
-                util.add_to_dict(skipped_counts, 'outside_of_exon')
-            break
-    return exon_num
